b576eb015ac0cb96869c8115d82486280a9be469
mingj7235 <joshuara7235@gmail.com>
2023-04-06 18:24:21 +0900

    feat : Infra-structure - ECR, ECS 설정 - task-definition config - GithubActions ci / cd workflows - clean artifact - prd yml data protection - dockerfile (api, nginx) - nginx configuration

--- src/test/java/webling/coffee/backend/user/repository/UserRepositoryTest.java
+++ src/test/java/webling/coffee/backend/user/repository/UserRepositoryTest.java
@@ -1,10 +1,8 @@
 package webling.coffee.backend.user.repository;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import webling.coffee.backend.domain.user.entity.User;
 import webling.coffee.backend.domain.user.repository.UserRepository;
 
 @SpringBootTest
@@ -18,11 +16,5 @@ public class UserRepositoryTest {
         String email = "joshua@snaps.com";
         String username = "조슈아";
 
-        userRepository.save(new User(email, username));
-
-        User user = userRepository.findByUsername(username);
-
-        Assertions.assertThat(user.getEmail()).isEqualTo(email);
-        Assertions.assertThat(user.getUsername()).isEqualTo(username);
     }
 }
