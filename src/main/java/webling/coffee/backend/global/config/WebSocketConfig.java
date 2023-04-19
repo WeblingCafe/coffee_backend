013696a0ef7a6e8fdf5db73f623351bba2910a15
mingj7235 <joshuara7235@gmail.com>
2023-04-19 17:43:32 +0900

    feat : WebSocket 관련 라이브러리 implementation

--- src/main/java/webling/coffee/backend/global/config/WebSocketConfig.java
+++ src/main/java/webling/coffee/backend/global/config/WebSocketConfig.java
@@ -1,14 +1,28 @@
 package webling.coffee.backend.global.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.stomp.StompFrameHandler;
+import org.springframework.messaging.simp.stomp.StompSessionHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @Configuration
-public class WebSocketConfig {
+@EnableWebSocketMessageBroker
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final StompSessionHandler stompFrameHandler;
 
     @Bean
     public ServerEndpointExporter serverEndpointExporter() {
         return new ServerEndpointExporter();
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
     }
 }
