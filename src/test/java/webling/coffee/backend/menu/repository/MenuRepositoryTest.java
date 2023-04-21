aaaa1136b79e6202d112ddc981629a019a4c8fb7
mingj7235 <joshuara7235@gmail.com>
2023-04-21 11:38:22 +0900

    chore : Menu 도메인 테스트 명세 init

---
+++ src/test/java/webling/coffee/backend/menu/repository/MenuRepositoryTest.java
@@ -0,0 +1,7 @@
+package webling.coffee.backend.menu.repository;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MenuRepositoryTest {
+}
---
+++ src/test/java/webling/coffee/backend/menu/service/MenuServiceTest.java
@@ -0,0 +1,44 @@
+package webling.coffee.backend.menu.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import webling.coffee.backend.domain.menu.service.core.MenuService;
+
+@SpringBootTest
+public class MenuServiceTest {
+
+    @Autowired
+    private MenuService menuService;
+
+    @Test
+    public void 메뉴를_등록한다 () {
+
+    }
+
+    @Test
+    public void 중복_메뉴_이름으로_메뉴_등록에_실패한다 () {
+
+    }
+
+
+    @Test
+    public void 메뉴를_변경한다 () {
+
+    }
+
+    @Test
+    public void 메뉴를_품절처리한다 () {
+
+    }
+
+    @Test
+    public void 메뉴를_한_건_조회한다 () {
+
+    }
+
+    @Test
+    public void 메뉴전체를_조회한다 () {
+
+    }
+}
