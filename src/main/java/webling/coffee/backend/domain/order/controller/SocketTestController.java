fec28b98a4cf725586da6bd919e2cd543bcbe8e8
mingj7235 <joshuara7235@gmail.com>
2023-05-15 18:34:28 +0900

    test : web socket 활용한 order 값 변경 감지 테스트

--- src/main/java/webling/coffee/backend/domain/order/controller/SocketTestController.java
+++ src/main/java/webling/coffee/backend/domain/order/controller/SocketTestController.java
@@ -1,14 +1,22 @@
 package webling.coffee.backend.domain.order.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 import webling.coffee.backend.domain.order.dto.request.ChatMessage;
+import webling.coffee.backend.domain.order.service.OrderFacade;
 
 @Controller
-public class AlarmSocketController {
+@RequiredArgsConstructor
+public class SocketTestController {
+
+    /**
+     * 주문이 들어오면 -> order 가 추가되면 chat message 에 변경이 일어나도록 request 없이! 로직을 짜볼것
+     */
+    private final OrderFacade orderFacade;
 
     @MessageMapping("/chat.sendMessage")
     @SendTo("/topic/public")
