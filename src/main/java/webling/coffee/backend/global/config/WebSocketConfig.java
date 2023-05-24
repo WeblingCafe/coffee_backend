6a8651dc67ef9e9e6c99b074280dca9fecbf5438
joshua <joshuara7235@gmail.com>
2023-05-24 23:03:31 +0900

    feat : websocket config allow origins path 추가

--- src/main/java/webling/coffee/backend/global/config/WebSocketConfig.java
+++ src/main/java/webling/coffee/backend/global/config/WebSocketConfig.java
@@ -1,6 +1,7 @@
 package webling.coffee.backend.global.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
@@ -16,16 +17,23 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     private final StompInterceptor stompInterceptor;
 
+    @Value("${cors.swagger}")
+    private String backendUrl;
+
+    @Value("${cors.frontend-local}")
+    private String frontendLocalUrl;
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/socket-coffee")
+                .setAllowedOrigins(backendUrl, frontendLocalUrl)
                 .withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app"); // @MessageMapping 어노테이션이 달린 메소드로 라우팅 된다.
-        registry.enableSimpleBroker("/topic");
+        registry.enableSimpleBroker("/");
     }
 
     @Override
