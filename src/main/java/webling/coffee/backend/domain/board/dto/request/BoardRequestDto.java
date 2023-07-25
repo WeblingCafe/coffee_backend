f766b158916736fdb1194133fc96cfc371539ec1
mingj7235 <joshuara7235@gmail.com>
2023-07-25 15:26:46 +0900

    feat : Board 조회 Pagination (issue #1)

--- src/main/java/webling/coffee/backend/domain/board/dto/request/BoardRequestDto.java
+++ src/main/java/webling/coffee/backend/domain/board/dto/request/BoardRequestDto.java
@@ -2,10 +2,14 @@ package webling.coffee.backend.domain.board.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import webling.coffee.backend.domain.board.dto.response.BoardResponseDto;
 import webling.coffee.backend.domain.board.entity.Board;
+
+import static java.lang.Math.max;
+import static java.lang.Math.min;
 
 public class BoardRequestDto {
 
@@ -30,5 +34,24 @@ public class BoardRequestDto {
         private String title;
         @NotNull
         private String content;
+    }
+
+    @Getter
+    @Setter
+    @Builder
+    @Schema (name = "boardRequestSearch")
+    public static class Search {
+
+        private static final int MAX_SIZE = 200;
+
+        @Builder.Default
+        private Integer page = 1;
+
+        @Builder.Default
+        private Integer size = 10;
+
+        public long getOffSet () {
+            return (long) (max(1, page) -1) * min(size, MAX_SIZE);
+        }
     }
 }
